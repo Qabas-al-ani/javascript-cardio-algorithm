947d7a1a76e13a85d0d59335ff272a43d23dbabe
Qabas <qabasalani1@gmail.com>
2022-04-16 11:34:53 -0400

    sort the array

--- challenges/index.js
+++ challenges/index.js
@@ -2,6 +2,10 @@
 // Solution word
 function longestWord(sen) {
   //   created filleted array
-
   const wordArr = sen.ToLowerCase().match(/[a-z0-9]+/g);
+
+  //   to sort the array
+  const sorted = wordArr.sort(function (a, b) {
+    return b.length - a.length;
+  });
 }
