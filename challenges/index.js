fc2f3989ff6ec5800cd771846a758ed97da0f27b
Qabas <qabasalani1@gmail.com>
2022-04-16 11:51:06 -0400

    challenge chunked arr

--- challenges/index.js
+++ challenges/index.js
@@ -1,24 +1,29 @@
-// longest word of a string
-// Solution word
-function longestWord(sen) {
-  //   created filleted array
-  const wordArr = sen.ToLowerCase().match(/[a-z0-9]+/g);
+// // longest word of a string
+// // Solution word
+// function longestWord(sen) {
+//   //   created filleted array
+//   const wordArr = sen.ToLowerCase().match(/[a-z0-9]+/g);
 
-  //   to sort the array
-  const sorted = wordArr.sort(function (a, b) {
-    return b.length - a.length;
-  });
+//   //   to sort the array
+//   const sorted = wordArr.sort(function (a, b) {
+//     return b.length - a.length;
+//   });
 
-  //   if multiple words put in an array
-  const longestWordArr = sorted.filter(function (word) {
-    return word.length === sorted[0].length;
-  });
+//   //   if multiple words put in an array
+//   const longestWordArr = sorted.filter(function (word) {
+//     return word.length === sorted[0].length;
+//   });
 
-  //   Check if there's more than one array value
-  if (longestWordArr.length === 1) {
-    return longestWordArr[0];
-  } else {
-    return longestWordArr;
-  }
-}
-console.log(longestWord("practice-algorithms, javascript-cardio-algoritms"));
+//   //   Check if there's more than one array value
+//   if (longestWordArr.length === 1) {
+//     return longestWordArr[0];
+//   } else {
+//     return longestWordArr;
+//   }
+// }
+// console.log(longestWord("Check if there's more than one array value"));
+
+///////////////////////////////
+// CHALLENGE Chunked array
+// Solution
+function ChunkedArray(arr, length) {}
